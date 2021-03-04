3c01caeac30eaa3e60c5b2b2ae46d4ddb275e160
arielmirra <amirra@mulesoft.com>
2021-03-04 16:22:42 -0300

    APIMF-2775: add resource loader example in js

--- src/test/java/ResourceLoaderTest.java
+++ src/test/java/ResourceLoaderTest.java
@@ -1,17 +1,22 @@
 import amf.MessageStyles;
-import amf.ProfileName;
 import amf.ProfileNames;
 import amf.client.AMF;
 import amf.client.environment.DefaultEnvironment;
 import amf.client.environment.Environment;
 import amf.client.model.document.BaseUnit;
 import amf.client.parse.Raml10Parser;
+import amf.client.remote.Content;
+import amf.client.resource.FileResourceLoader;
+import amf.client.resource.ResourceLoader;
 import amf.client.validate.ValidationReport;
-import aml_org.CustomResourceLoader;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -20,6 +25,24 @@ public class ResourceLoaderTest {
     @BeforeClass
     public static void setup() throws ExecutionException, InterruptedException {
         AMF.init().get();
+    }
+
+    private static class CustomResourceLoader implements ResourceLoader {
+        private final FileResourceLoader resourceLoader = new FileResourceLoader();
+        private final Pattern CUSTOM_PATH_PATTERN = Pattern.compile("^CustomProtocol/");
+
+        @Override
+        public CompletableFuture<Content> fetch(String path) {
+            final String normalizedPath = path.substring(CUSTOM_PATH_PATTERN.pattern().length() - 1);
+            return resourceLoader.fetch(new File(normalizedPath).getAbsolutePath());
+        }
+
+        @Override
+        public boolean accepts(String resource) {
+            if (resource == null || resource.isEmpty()) return false;
+            final Matcher matcher = CUSTOM_PATH_PATTERN.matcher(resource);
+            return matcher.find();
+        }
     }
 
     @Test
