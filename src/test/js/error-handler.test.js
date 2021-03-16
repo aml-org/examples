b4d9f1b575e783c136ce0cb691270e418b622715
arielmirra <amirra@mulesoft.com>
2021-03-16 13:43:51 -0300

    APIMF-2776: Error Handler tests

---
+++ src/test/js/error-handler.test.js
@@ -0,0 +1,12 @@
+const amf = require('amf-client-js');
+
+beforeAll(() => {
+    amf.AMF.init();
+
+});
+
+test('unhandled error handler', () => {
+
+    // Support for Custom Error Handlers in Javascript is in development stage.
+
+})
--- src/test/js/resource-loader.test.js
+++ src/test/js/resource-loader.test.js
@@ -5,27 +5,27 @@ beforeAll(() => {
 
 });
 
-test('Validate RAML 1.0 with custom resource loader', () => {
-    class CustomResourceLoader {
-        static CUSTOM_PATTERN = "CustomProtocol";
+class CustomResourceLoader {
+    static CUSTOM_PATTERN = "CustomProtocol";
 
-        constructor() {
-            this.resourceLoader = new amf.JsServerFileResourceLoader();
-        }
-
-        // has the logic for fetching in a custom way
-        fetch(resource) {
-            const customLogicResult = resource.toString().replace(CustomResourceLoader.CUSTOM_PATTERN, 'file:/');
-            return this.resourceLoader.fetch(customLogicResult);
-        }
-
-        // returns true if it can fetch the resource
-        accepts(resource) {
-            if (resource === null || resource === undefined || resource === "") return false;
-            return resource.startsWith(CustomResourceLoader.CUSTOM_PATTERN);
-        }
+    constructor() {
+        this.resourceLoader = new amf.JsServerFileResourceLoader();
     }
 
+    // has the logic for fetching in a custom way
+    fetch(resource) {
+        const customLogicResult = resource.toString().replace(CustomResourceLoader.CUSTOM_PATTERN, 'file:/');
+        return this.resourceLoader.fetch(customLogicResult);
+    }
+
+    // returns true if it can fetch the resource
+    accepts(resource) {
+        if (resource === null || resource === undefined || resource === "") return false;
+        return resource.startsWith(CustomResourceLoader.CUSTOM_PATTERN);
+    }
+}
+
+test('Validate RAML 1.0 with custom resource loader', () => {
     const env = amf.client.DefaultEnvironment.apply().addClientLoader(new CustomResourceLoader());
     const parser = new amf.Raml10Parser(env);
 
