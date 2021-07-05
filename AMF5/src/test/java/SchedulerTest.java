c3089bae993ea45a3d20a82907be7ca436297051
Agustin Bettati <bettatiagustin@gmail.com>
2021-07-05 14:56:21 -0300

    APIMF-3203: include all async operations for scheduler test

--- AMF5/src/test/java/SchedulerTest.java
+++ AMF5/src/test/java/SchedulerTest.java
@@ -1,10 +1,19 @@
 import amf.apicontract.client.platform.AMFConfiguration;
 import amf.apicontract.client.platform.RAMLConfiguration;
+import amf.apicontract.client.platform.model.domain.EndPoint;
+import amf.apicontract.client.platform.model.domain.Operation;
+import amf.apicontract.client.platform.model.domain.Payload;
+import amf.apicontract.client.platform.model.domain.Request;
+import amf.apicontract.client.platform.model.domain.api.WebApi;
+import amf.core.client.common.validation.ValidationMode;
 import amf.core.client.platform.AMFGraphClient;
 import amf.core.client.platform.AMFGraphConfiguration;
 import amf.core.client.platform.AMFResult;
 import amf.core.client.platform.execution.ExecutionEnvironment;
+import amf.core.client.platform.model.document.BaseUnit;
+import amf.core.client.platform.model.document.Document;
 import amf.core.client.platform.validation.AMFValidationReport;
+import amf.shapes.client.platform.model.domain.AnyShape;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
@@ -35,13 +44,25 @@ public class SchedulerTest {
                 .withExecutionEnvironment(executionEnvironment);
         final AMFGraphClient client = config.createClient();
 
-        /* Parsing Raml 10 with specified file returning future. */
-        final AMFResult parseResult = client.parse("file://resources/examples/banking-api.raml").get();
+        /* call async interfaces */
 
-        /* Validating the Raml 10 spec. */
+        final AMFResult parseResult = client.parse("file://resources/examples/simple-api.raml").get();
         AMFValidationReport report = client.validate(parseResult.baseUnit()).get();
+        AMFValidationReport payloadReport = config.payloadValidatorFactory()
+                .createFor(obtainShapeFromUnit(parseResult.baseUnit()), "application/json", ValidationMode.StrictValidationMode())
+                .validate("{\"name\": \"firstname and lastname\"}").get();
+//        config.forInstance
 
         /* Shutting down the scheduler which kills the AMF threads created in the thread pool provided by that scheduler. */
         scheduler.shutdownNow();
+    }
+
+    private AnyShape obtainShapeFromUnit(BaseUnit b) {
+        final WebApi webApi = (WebApi) ((Document) b).encodes();
+        final EndPoint usersEndpoint = webApi.endPoints().get(0);
+        final Operation postMethod = usersEndpoint.operations().get(0);
+        final Request request = postMethod.requests().get(0);
+        final Payload userPayload = request.payloads().get(0);
+        return (AnyShape) userPayload.schema();
     }
 }
--- AMF5/src/test/scala/SchedulerTestScala.scala
+++ AMF5/src/test/scala/SchedulerTestScala.scala
@@ -1,18 +1,30 @@
 import amf.apicontract.client.scala.RAMLConfiguration
+import amf.apicontract.client.scala.model.domain.api.WebApi
+import amf.core.client.common.validation.ValidationMode
 import amf.core.client.scala.AMFGraphConfiguration
 import amf.core.client.scala.errorhandling.UnhandledErrorHandler
 import amf.core.client.scala.execution.ExecutionEnvironment
+import amf.core.client.scala.model.document.{BaseUnit, Document}
+import amf.core.client.scala.model.domain.Shape
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{Executors, ThreadFactory}
 import scala.concurrent.ExecutionContext
 
 class SchedulerTestScala extends AsyncFlatSpec with should.Matchers {
 
   "AMF configuration" should "allow defining custom execution context" in {
     /* Instantiating a ScheduledExecutorService with some example arguments. */
-    val scheduler = Executors.newScheduledThreadPool(30);
+    val scheduler = Executors.newScheduledThreadPool(30, new ThreadFactory {
+      var i: Int = 0
+      override def newThread(r: Runnable): Thread = {
+        i = i + 1
+        val thread = new Thread(r);
+        thread.setName("AMF-" + i);
+        thread
+      }
+    });
 
     /* Instantiating a ExecutionEnvironment passing as argument the scheduler previously created. */
     val executionEnvironment = new ExecutionEnvironment(ExecutionContext.fromExecutorService(scheduler));
@@ -21,13 +33,31 @@ class SchedulerTestScala extends AsyncFlatSpec with should.Matchers {
       .withExecutionEnvironment(executionEnvironment);
     val client = config.createClient();
 
+    /* call async interfaces */
     for {
-      parseResult <-client.parse("file://resources/examples/banking-api.raml")
+      parseResult      <- client.parse("file://resources/examples/simple-api.raml")
       validationResult <- client.validate(parseResult.bu)
+      payloadReport <- config
+        .payloadValidatorFactory()
+        .createFor(obtainShapeFromUnit(parseResult.bu), "application/json", ValidationMode.StrictValidationMode)
+        .validate("{\"name\": \"firstname and lastname\"}")
+//      newConfig <- config.forInstance("")
     } yield {
       /* Shutting down the scheduler which kills the AMF threads created in the thread pool provided by that scheduler. */
       scheduler.shutdownNow();
       succeed
     }
+  }
+
+  def obtainShapeFromUnit(b: BaseUnit): Shape = {
+    val webApi = b
+      .asInstanceOf[Document]
+      .encodes
+      .asInstanceOf[WebApi]
+    val usersEndpoint = webApi.endPoints.head
+    val postMethod    = usersEndpoint.operations.head
+    val request       = postMethod.requests.head
+    val userPayload   = request.payloads.head
+    userPayload.schema
   }
 }
