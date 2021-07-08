54cf6690de81d72109f6500b0f102359a0575037
Agustin Bettati <bettatiagustin@gmail.com>
2021-07-08 12:20:06 -0300

    examples for parsing and render options

---
+++ AMF5/src/test/java/javaPlatform/ConfigurationOptionsTest.java
@@ -0,0 +1,35 @@
+package javaPlatform;
+
+import amf.apicontract.client.common.ProvidedMediaType;
+import amf.apicontract.client.platform.AMFClient;
+import amf.apicontract.client.platform.OASConfiguration;
+import amf.apicontract.client.platform.WebAPIConfiguration;
+import amf.core.client.platform.AMFResult;
+import amf.core.client.platform.config.ParsingOptions;
+import amf.core.client.platform.config.RenderOptions;
+import amf.core.client.platform.model.document.BaseUnit;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConfigurationOptionsTest {
+
+    @Test
+    public void configurationWithOptions() throws ExecutionException, InterruptedException, IOException {
+        AMFClient client = OASConfiguration.OAS20()
+                .withParsingOptions(new ParsingOptions().setMaxYamlReferences(20))
+                .withRenderOptions(new RenderOptions().withSourceMaps())
+                .createClient();
+
+        final AMFResult parseResult = client.parse("file://resources/examples/banking-api.json").get();
+        final String result = client.render(parseResult.baseUnit()).trim();
+        assert(result.contains("[(87,12)-(87,39)]"));
+    }
+}
